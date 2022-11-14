ca1f2db7427b49d4615bb3085a7b87db89c9c508
Phase <82134074+phasenoisepon@users.noreply.github.com>
2022-11-14 02:39:54 -0600

    rename files and add docs

--- src/counter.v
+++ src/counter.v
@@ -1,6 +1,6 @@
 `default_nettype none
 
-module seven_segment_seconds #( parameter MAX_COUNT = 1000 ) (
+module phasenoisepon_seven_segment_seconds #( parameter MAX_COUNT = 1000 ) (
   input [7:0] io_in,
   output [7:0] io_out
 );
